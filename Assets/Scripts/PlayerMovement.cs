fe25c00e9355a944fa3f07cc952f0a398aff3e39
ArhamAzhar1 <arham8718290@gmail.com>
2024-07-26 20:27:19 +0500

    Scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,26 +2,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float forwardForce = 2000f;
 
-    // Start is called before the first frame update
-   
+	// This is a reference to the Rigidbody component called "rb"
+	public Rigidbody rb;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+	public float forwardForce = 2000f;  // Variable that determines the forward force
+	public float sidewaysForce = 500f;  // Variable that determines the sideways force
 
-     if (Input.GetKey("d"))
-     {
-        rb.AddForce(500 * Time.deltaTime, 0, 0);
-     }
+	// We marked this as "Fixed"Update because we
+	// are using it to mess with physics.
+	void FixedUpdate()
+	{
+		// Add a forward force
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-500 * Time.deltaTime, 0, 0);
-        }
+		if (Input.GetKey("d"))  // If the player is pressing the "d" key
+		{
+			// Add a force to the right
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
 
-    }
+		if (Input.GetKey("a"))  // If the player is pressing the "a" key
+		{
+			// Add a force to the left
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+	}
 }
